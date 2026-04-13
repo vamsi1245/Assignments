9225c315a98666348187813acf7c869101915a1a
vamsi krishna <vamsi1040v@gmail.com>
2026-04-13 13:05:11 +0530

    message

---
+++ javascript-training/language-basics/debug.js
@@ -0,0 +1,16 @@
+console.log("Executing line 1");
+console.log("Executing line 2");
+console.log("Executing line 3");
+sumofNumbers(5,10);
+console.log("Executing line 5");
+console.log("Executing line 6");
+
+
+
+function sumofNumbers(a,b){
+    c=a-b;
+    console.log(c);
+}
+
+//debug the prgm step by step
+
